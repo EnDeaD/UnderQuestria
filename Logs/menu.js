74eccb384794f94f4c275b64f27d41b5fbc5d19e
DiscordingMyself <33711547+EnDeaD@users.noreply.github.com>
2019-02-21 22:18:44 +0900

    Update menu.js

--- Logs/menu.js
+++ Logs/menu.js
@@ -1,6 +1,9 @@
 var Menu = {
     preload: function () {
-        game.load.image('btn', 'triggers/trigger.png');
+        game.load.image('btn0', 'assets/love.png');
+        game.load.image('btn1', 'assets/night.png');
+        game.load.image('btn2', 'assets/sun.png');
+        game.load.image('btn3', 'assets/twi.png');
         game.load.image('bg', 'assets/menu.png');
         game.load.image('name', 'assets/game_name.png');
     },
@@ -14,22 +17,22 @@ var Menu = {
         this.name = game.add.sprite(140, 30, 'name');
         this.name.scale.setTo(0.6)
         
-        this.btn = game.add.sprite(340, 275, 'btn');
+        this.btn = game.add.sprite(340, 275, 'btn0');
         this.btn.inputEnabled = true;
         this.btn.scale.setTo(0.355, 0.125)
         this.btn.events.onInputDown.add(this.startGame);
         
-        this.word = game.add.sprite(10, 530, 'btn');
+        this.word = game.add.sprite(10, 530, 'btn1');
         this.word.inputEnabled = true;
         this.word.scale.setTo(0.2)
         this.word.events.onInputDown.add(this.words);
 
-        this.inf = game.add.sprite(730, 530, 'btn');
+        this.inf = game.add.sprite(730, 530, 'btn2');
         this.inf.inputEnabled = true;
         this.inf.scale.setTo(0.2)
         this.inf.events.onInputDown.add(this.info);
 
-        this.mor = game.add.sprite(730, 10, 'btn');
+        this.mor = game.add.sprite(730, 10, 'btn3');
         this.mor.inputEnabled = true;
         this.mor.scale.setTo(0.2)
         this.mor.events.onInputDown.add(this.more);
