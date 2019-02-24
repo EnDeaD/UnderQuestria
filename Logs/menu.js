4681dce8353789102cb95c4fc803dd19829e8674
DiscordingMyself <33711547+EnDeaD@users.noreply.github.com>
2019-02-24 22:27:44 +0900

    Update menu.js

--- Logs/menu.js
+++ Logs/menu.js
@@ -33,7 +33,7 @@ var Menu = {
         this.btn.scale.setTo(0.333, 0.1);
         this.btn.events.onInputDown.add(this.startGame);
         
-        this.text = game.add.text(200, 575, 'UNDERQUESTRIA V0.2 (C) DISCORDINGAMES 2019', { fontSize: '15px', fill: '#999', font: 'bold 15pt Pixel', fontStyle: 'comic cans' });
+        this.text = game.add.text(200, 575, 'UNDERQUESTRIA V0.2 (C) DISCORDINGAMES 2019', { fontSize: '15px', fill: '#999', font: 'bold 30pt Pixel', fontStyle: 'comic cans' });
         this.text.inputEnabled = true;
         this.text = game.add.text(200, 200, 'Home - Somewhere', { fontSize: '30px', fill: '#FFF', font: 'bold 30pt Pixel' });
         this.text.inputEnabled = true;
