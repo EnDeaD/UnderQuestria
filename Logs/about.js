de47042e15887e0a168801d051ded47bb3eebf83
DiscordingMyself <33711547+EnDeaD@users.noreply.github.com>
2019-02-24 21:54:06 +0900

    Update about.js

--- Logs/about.js
+++ Logs/about.js
@@ -11,11 +11,11 @@ var About = {
     create: function() {
         this.dialoge = game.add.sprite(0, -900, 'dialoge');
         this.dialoge.scale.setTo(1.7, 5)
-        this.text0 = game.add.text(75, 75, 'О нас:', { fontSize: '30px', fill: '#FFF', font: 'bold 20pt sans' });
-        this.text1 = game.add.text(75, 125, 'Автор: Discording Myself', { fontSize: '24px', fill: '#FFF', font: 'bold 20pt Sans' });
-        this.text2 = game.add.text(75, 175, 'Наш сайт: всё ещё в разработке', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt Arial' });
+        this.text0 = game.add.text(75, 75, 'About us:', { fontSize: '30px', fill: '#FFF', font: 'bold 20pt sans' });
+        this.text1 = game.add.text(75, 125, 'Created by: Discording Myself', { fontSize: '24px', fill: '#FFF', font: 'bold 20pt Sans' });
+        this.text2 = game.add.text(75, 175, 'Website: still developing', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt Arial' });
         this.text3 = game.add.text(75, 225, '', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt Arial' });
-        this.text4 = game.add.text(75, 275, 'Вдохновленно: July Allshine (пони с главного экрана)', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt Arial' });
+        this.text4 = game.add.text(75, 275, 'Inspired by: July Allshine (пони с главного экрана)', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt Arial' });
         this.text5 = game.add.text(75, 325, '', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
         this.text6 = game.add.text(75, 375, '', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
         this.text7 = game.add.text(75, 425, '', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
