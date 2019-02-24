de7006d057bb56db2d132adab8a4f06b2c631ef0
DiscordingMyself <33711547+EnDeaD@users.noreply.github.com>
2019-02-24 21:44:42 +0900

    Update about.js

--- Logs/about.js
+++ Logs/about.js
@@ -12,13 +12,13 @@ var About = {
         this.dialoge = game.add.sprite(0, -900, 'dialoge');
         this.dialoge.scale.setTo(1.7, 5)
         this.text0 = game.add.text(75, 75, 'О нас:', { fontSize: '30px', fill: '#FFF', font: 'bold 20pt sans' });
-        this.text1 = game.add.text(75, 125, 'IT-Labor(TM)', { fontSize: '24px', fill: '#FFF', font: 'bold 20pt sans' });
-        this.text2 = game.add.text(75, 175, 'Наш сайт: www.it-labor.ru', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
-        this.text3 = game.add.text(75, 225, 'Работу выполнил: Комин Александр', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
-        this.text4 = game.add.text(75, 275, 'Под руководством: Зайкова Максима Андреевича', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
+        this.text1 = game.add.text(75, 125, 'Автор: Discording Myself', { fontSize: '24px', fill: '#FFF', font: 'bold 20pt Sans' });
+        this.text2 = game.add.text(75, 175, 'Наш сайт: всё ещё в разработке', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt Arial', fontStyle: eg.'ComicSans' });
+        this.text3 = game.add.text(75, 225, '', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt Arial' });
+        this.text4 = game.add.text(75, 275, 'Вдохновленно: July Allshine (пони с главного экрана)', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt Arial' });
         this.text5 = game.add.text(75, 325, '', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
         this.text6 = game.add.text(75, 375, '', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
-        this.text6 = game.add.text(75, 425, '', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
+        this.text7 = game.add.text(75, 425, '', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
         game.input.onDown.add(this.clicked, this);
     },
     update: function() {
