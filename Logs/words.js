dce85a6b2e879733bf930f0f86996601c41a0daa
DiscordingMyself <33711547+EnDeaD@users.noreply.github.com>
2019-03-27 17:19:59 +0900

    Update words.js

--- Logs/words.js
+++ Logs/words.js
@@ -12,14 +12,13 @@ var Words = {
         this.dialoge = game.add.sprite(0, -900, 'dialoge');
         this.dialoge.scale.setTo(1.7, 5)
         this.text0 = game.add.text(75, 75, 'Заметка от автора:', { fontSize: '30px', fill: '#FFF', font: 'bold 20pt sans' });
-        this.text1 = game.add.text(75, 125, 'Эта фан-игра ("Пародия" - п. 3, ст. 1274 ГК РФ), на', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
-        this.text2 = game.add.text(75, 175, 'оригинальную игру Undertale by TobyFox.', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
-        this.text3 = game.add.text(75, 225, 'Я приношу свои извинения:', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
-        this.text4 = game.add.text(75, 275, '*За использование спрайтов оригинальной игры;', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
-        this.text5 = game.add.text(75, 325, '*За изменение механики и концепта игры;', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
-        this.text6 = game.add.text(75, 375, '*За оскорбление чьих-либо чувств.', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
-        this.text6 = game.add.text(75, 425, '*За грамматические и пунктационные ошибки.', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
-        this.text = game.add.text(game.world.width-150, game.world.height-100, '-Автор', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
+        this.text1 = game.add.text(75, 125, 'Эта фан-игра ("Пародия" - п. 3, ст. 1274 ГК РФ),', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
+        this.text2 = game.add.text(75, 175, 'на оригинальную игру Undertale by TobyFox.', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
+        this.text3 = game.add.text(75, 225, 'Автор приносит свои извинения за:', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
+        this.text4 = game.add.text(75, 275, '*Использование спрайтов оригинальной игры;', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
+        this.text5 = game.add.text(75, 325, '*Изменение механики и концепта игры;', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
+        this.text6 = game.add.text(75, 375, '*Оскорбление чьих-либо чувств.', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
+        this.text6 = game.add.text(75, 425, '*Грамматические и пунктационные ошибки.', { fontSize: '24px', fill: '#FFF', font: 'regular 64pt sans' });
         game.input.onDown.add(this.clicked, this);
     },
     update: function() {
