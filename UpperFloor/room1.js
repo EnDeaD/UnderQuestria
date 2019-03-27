f0ebc42aaa6613408ea09b8738cb8c0c89774e71
DiscordingMyself <33711547+EnDeaD@users.noreply.github.com>
2019-03-27 23:10:06 +0900

    Update room1.js

--- UpperFloor/room1.js
+++ UpperFloor/room1.js
@@ -128,7 +128,7 @@ var Room1 = {
     unlocked: function () {
         Room1.dialoge = game.add.sprite(0, 0, 'dialoge');
         Room1.dialoge.scale.setTo(1)
-        Room1.text1 = game.add.text(75, 40, '* Дверь теперь открыта...', { fontSize: '20px', fill: '#FFF', font: 'bold 20pt sans' });
+        Room1.text1 = game.add.text(75, 40, '* Теперь Вы можете открыть дверь...', { fontSize: '20px', fill: '#FFF', font: 'bold 20pt sans' });
         Room1.dialoge.inputEnabled = true;
         Room1.dialoge.events.onInputDown.add(Room1.delete);
         room1un = true;
