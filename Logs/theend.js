d46dbfeb31ea3697cb1b912cfa084f99b94bd33f
DiscordingMyself <33711547+EnDeaD@users.noreply.github.com>
2019-03-27 19:17:42 +0900

    Update theend.js

--- Logs/theend.js
+++ Logs/theend.js
@@ -10,7 +10,7 @@ var TheEnd = {
     dialoge: null,
     create: function() {
         this.dialoge = game.add.sprite(0, 0, 'dialoge');
-        this.dialoge.scale.setTo(1)
+        this.dialoge.scale.setTo(1, 3)
         this.text0 = game.add.text(200, 225, 'КОНЕЦ.', { fontSize: '100px', fill: '#FFF', font: 'bold 20pt Sans' });
         game.input.onDown.add(this.clicked, this);
     },
