7dd9b7fefea2868e2cb4a549c899956f4dff381a
DiscordingMyself <33711547+EnDeaD@users.noreply.github.com>
2019-03-27 18:24:49 +0900

    Update theend.js

--- Logs/theend.js
+++ Logs/theend.js
@@ -1,6 +1,6 @@
 var TheEnd = {
     preload: function() {
-        game.load.image('dialoge', 'assets/dialoge.jpg');
+        game.load.image('dialoge', 'assets/dialog.png');
     },
     text1: null,
     text2: null,
