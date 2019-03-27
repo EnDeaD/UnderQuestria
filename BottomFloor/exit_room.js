c0cf345ca554baa63ce9b0d26e8d5663ff53f925
DiscordingMyself <33711547+EnDeaD@users.noreply.github.com>
2019-03-27 19:19:12 +0900

    Update exit_room.js

--- BottomFloor/exit_room.js
+++ BottomFloor/exit_room.js
@@ -10,12 +10,12 @@ var Exit = {
     dialoge: null,
     create: function() {
         game.stage.backgroundColor = "#000";
-        this.dialoge = game.add.sprite(0, -300, 'dialoge');
-        this.dialoge.scale.setTo(1.7)
+        this.dialoge = game.add.sprite(0, 0, 'dialoge');
+        this.dialoge.scale.setTo(1)
         this.text1 = game.add.text(75, 40, '* Вы выбрались...', { fontSize: '20px', fill: '#FFF', font: 'bold 20pt sans' });
         game.input.onDown.add(this.clicked, this);
-        this.background = game.add.sprite(-1, 225, 'bg');
-        this.background.scale.setTo(1.35)
+        this.background = game.add.sprite(0, 0, 'bg');
+        this.background.scale.setTo(1)
     },
     update: function() {
 
