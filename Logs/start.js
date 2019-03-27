8a85d2c4eb4f2596c21f5927cbacb2d5549defb6
DiscordingMyself <33711547+EnDeaD@users.noreply.github.com>
2019-03-27 17:54:33 +0900

    Update start.js

--- Logs/start.js
+++ Logs/start.js
@@ -1,6 +1,6 @@
 var Start = {
     preload: function() {
-        game.load.image('dialoge', 'assets/dialoge.jpg');
+        game.load.image('dialoge', 'assets/dialog.png');
     },
     text1: null,
     text2: null,
@@ -8,8 +8,8 @@ var Start = {
     text4: null,
     dialoge: null,
     create: function() {
-        this.dialoge = game.add.sprite(0, -300, 'dialoge');
-        this.dialoge.scale.setTo(1.7)
+        this.dialoge = game.add.sprite(0, 0, 'dialoge');
+        this.dialoge.scale.setTo(1)
         this.text1 = game.add.text(75, 40, '* Вы - юная пони, просыпаетесь в незнакомом вам месте.', { fontSize: '20px', fill: '#FFF', font: 'bold 20pt sans' });
         this.text2 = game.add.text(75, 80, 'Ваша задача: выбраться из этого дома и, по возможности,', { fontSize: '20px', fill: '#FFF', font: 'bold 64pt sans' });
         this.text3 = game.add.text(75, 120, 'узнать все его тайны...', { fontSize: '20px', fill: '#FFF', font: 'bold 64pt sans' });
