fdd0a743cf4be173b565fa0e26697e80c84d7179
DiscordingMyself <33711547+EnDeaD@users.noreply.github.com>
2019-03-27 19:41:45 +0900

    Update start.js

--- Logs/start.js
+++ Logs/start.js
@@ -11,9 +11,9 @@ var Start = {
         this.dialoge = game.add.sprite(0, 0, 'dialoge');
         this.dialoge.scale.setTo(1)
         this.text1 = game.add.text(75, 40, '* Вы - юная пони, просыпаетесь в незнакомом вам месте.', { fontSize: '20px', fill: '#FFF', font: 'bold 20pt sans' });
-        this.text2 = game.add.text(75, 80, 'Ваша задача: выбраться из этого дома и, по возможности,', { fontSize: '20px', fill: '#FFF', font: 'bold 64pt sans' });
-        this.text3 = game.add.text(75, 120, 'узнать все его тайны...', { fontSize: '20px', fill: '#FFF', font: 'bold 64pt sans' });
-        this.text4 = game.add.text(75, 150, '(Старайтесь найти все секреты, чтобы узнать истину...)', { fontSize: '20px', fill: '#090909', font: 'bold 64pt sans' });
+        this.text2 = game.add.text(75, 80, 'Ваша задача: выбраться из этого дома и, по возможности,', { fontSize: '20px', fill: '#FFF', font: 'bold 20pt sans' });
+        this.text3 = game.add.text(75, 120, 'узнать все его тайны...', { fontSize: '20px', fill: '#FFF', font: 'bold 20pt sans' });
+        this.text4 = game.add.text(75, 150, '(Старайтесь найти все секреты, чтобы узнать истину...)', { fontSize: '20px', fill: '#090909', font: 'bold 20pt sans' });
         this.dialoge.inputEnabled = true;
         this.dialoge.events.onInputDown.add(this.clicked1);
     },
