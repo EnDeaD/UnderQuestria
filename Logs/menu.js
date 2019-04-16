0b31ec618e530ffd9cec4d518e188271c9e11fdb
DiscordingMyself <33711547+EnDeaD@users.noreply.github.com>
2019-04-16 19:18:23 +0900

    Update menu.js

--- Logs/menu.js
+++ Logs/menu.js
@@ -41,7 +41,7 @@ var Menu = {
         }
         
         this.text = game.add.text(210, 580, 'UNDERQUESTRIA V0.45 (C) DISCORDINGAMES 2019', { fontSize: '15px', fill: '#808080', font: 'bold 15pt Pixel' });
-        this.text = game.add.text(175, 560, '"UNDERTALE" & "MY LITTLE PONY" Сrossover (FAN-MADE)', { fontSize: '15px', fill: '#404040', font: 'bold 15pt Pixel' });
+        this.text = game.add.text(175, 560, '"UNDERTALE" & "MY LITTLE PONY" Сrossover (FAN-MADE)', { fontSize: '15px', fill: '#202020', font: 'bold 15pt Pixel' });
         this.text = game.add.text(200, 200, 'Home - Somewhere', { fontSize: '30px', fill: '#FFF', font: 'bold 30pt Pixel' });
         this.text = game.add.text(200, 150, 'July', { fontSize: '30px', fill: '#FFF', font: 'bold 30pt Pixel' });
         this.text = game.add.text(375, 150, 'LV 0', { fontSize: '30px', fill: '#FFF', font: 'bold 30pt Pixel' });
