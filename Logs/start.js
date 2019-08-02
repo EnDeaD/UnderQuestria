495aec28037006ad99025798827a84f620fded51
DiscordingMyself <33711547+EnDeaD@users.noreply.github.com>
2019-08-02 17:00:41 +0900

    Update start.js

--- Logs/start.js
+++ Logs/start.js
@@ -13,7 +13,7 @@ var Start = {
         this.text1 = game.add.text(75, 40, '* Вы - юная пони, просыпаетесь в незнакомом вам месте.', { fontSize: '20px', fill: '#FFF', font: 'bold 20pt sans' });
         this.text2 = game.add.text(75, 80, 'Ваша задача: выбраться из этого дома и, по возможности,', { fontSize: '20px', fill: '#FFF', font: 'bold 20pt sans' });
         this.text3 = game.add.text(75, 120, 'узнать все его тайны...', { fontSize: '20px', fill: '#FFF', font: 'bold 20pt sans' });
-        this.text4 = game.add.text(75, 150, '(Старайтесь найти все секреты, чтобы узнать истину...)', { fontSize: '20px', fill: '#090909', font: 'bold 20pt sans' });
+        this.text4 = game.add.text(75, 160, '(Старайтесь найти все секреты, чтобы узнать истину...)', { fontSize: '20px', fill: '#FFF', font: 'bold 20pt sans' });
         this.dialoge.inputEnabled = true;
         this.dialoge.events.onInputDown.add(this.clicked1);
     },
